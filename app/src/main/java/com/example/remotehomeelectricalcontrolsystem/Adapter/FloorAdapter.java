82e38734b04cab1226fcc50edee64fee6acd6aa4
MinhHien39 <tranminhhien06072001@gmail.com>
2023-10-23 14:22:10 +0700

    C6.1 - Update Room

--- app/src/main/java/com/example/remotehomeelectricalcontrolsystem/Adapter/FloorAdapter.java
+++ app/src/main/java/com/example/remotehomeelectricalcontrolsystem/Adapter/FloorAdapter.java
@@ -48,19 +48,47 @@ public class FloorAdapter extends RecyclerView.Adapter<FloorAdapter.ViewHolder>{
         holder.txtNameRoom.setText(roomList.get(position).getNameRoom());
         holder.txtCountRoom.setText(String.valueOf(roomList.get(position).getList().size()) + " " +"Device");
         //Log.i("txtCount" , String.valueOf(roomList.get(position).getList().size()));
-        holder.aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    
-                }
-            }
-        });
 
-        String url = roomList.get(position).getImgUrl();
-        Glide.with(holder.itemView.getContext())
-                .load(url)
-                .into(holder.imgRoom);
+
+        String url;
+        switch (roomList.get(position).getNameRoom()){
+            case "Living room":
+                url = "https://saigonanhkiet.com/wp-content/uploads/2018/08/thiet-ke-noi-that-kieu-nhat-khong-kho.jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgRoom);
+                break;
+            case "Kitchen room":
+                url = "https://tubepthongminh.com/wp-content/uploads/2019/06/noi-that-nha-bep-nhat-1.jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgRoom);
+                break;
+            case "Room 1":
+                url = "https://www.cleanipedia.com/images/5iwkm8ckyw6v/V0dmJdHrd2rWg0ucsXlB7/f953ad2d6713fb56ec303f253eb9e56f/dGhpZXQta2UtcGhvbmctbmd1LWtpZXUtbmhhdC03LmpwZw/990w-660h/thi%E1%BA%BFt-k%E1%BA%BF-ph%C3%B2ng-ng%E1%BB%A7-ki%E1%BB%83u-nh%C3%A2t.jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgRoom);
+                break;
+            case "Bathroom":
+                url = "https://viphouse.vn/viphouse/images/2015/t11/thiet%20ke%20phong%20tam%20phong%20cach%20nhat%20ban/Thiet_ke_phong_tam_phong_cach_nhat_ban_viphouse_vn%20(5).jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgRoom);
+                break;
+            case "Room 2":
+                String url1 = "https://www.cleanipedia.com/images/5iwkm8ckyw6v/V0dmJdHrd2rWg0ucsXlB7/f953ad2d6713fb56ec303f253eb9e56f/dGhpZXQta2UtcGhvbmctbmd1LWtpZXUtbmhhdC03LmpwZw/990w-660h/thi%E1%BA%BFt-k%E1%BA%BF-ph%C3%B2ng-ng%E1%BB%A7-ki%E1%BB%83u-nh%C3%A2t.jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url1)
+                        .into(holder.imgRoom);
+                break;
+            case "Yard":
+                url = "https://sanvuonadong.vn/wp-content/uploads/2020/07/san-vuon-nhat-ban-01-san-vuon-a-dong.jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgRoom);
+
+        }
         holder.imgRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,7 +114,7 @@ public class FloorAdapter extends RecyclerView.Adapter<FloorAdapter.ViewHolder>{
         TextView txtIdRoom;
         ImageView imgRoom;
         TextView txtCountRoom;
-        Switch aSwitch;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtNameRoom = itemView.findViewById(R.id.txtNameRoom);
@@ -94,7 +122,7 @@ public class FloorAdapter extends RecyclerView.Adapter<FloorAdapter.ViewHolder>{
 
             imgRoom = itemView.findViewById(R.id.imgRoom);
             txtCountRoom = itemView.findViewById(R.id.txtCountRoom);
-            aSwitch = itemView.findViewById(R.id.sw);
+
         }
     }
 }
--- app/src/main/java/com/example/remotehomeelectricalcontrolsystem/Adapter/RoomAdapter.java
+++ app/src/main/java/com/example/remotehomeelectricalcontrolsystem/Adapter/RoomAdapter.java
@@ -1,9 +1,12 @@
 package com.example.remotehomeelectricalcontrolsystem.Adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.remotehomeelectricalcontrolsystem.Model.Devices;
 import com.example.remotehomeelectricalcontrolsystem.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,10 +40,39 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull RoomAdapter.ViewHolder holder, int position) {
         holder.txtNameDevice.setText(devicesList.get(position).getNameDevice());
-        String url = devicesList.get(position).getImgUrl();
-        Glide.with(holder.itemView.getContext())
-                .load(url)
-                .into(holder.imgDevice);
+        String url;
+
+        switch (devicesList.get(position).getNameDevice()){
+            case "Fan" :
+                url = "https://quattrantamanh.com/wp-content/uploads/2022/03/1330..jpg";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgDevice);
+                break;
+            case "Light 1":
+                url = "https://bizweb.dktcdn.net/100/116/589/products/led-bulb-day-toc-rang-dong.png?v=1617004880860";
+                Glide.with(holder.itemView.getContext())
+                        .load(url)
+                        .into(holder.imgDevice);
+                break;
+            case "Light 2":
+                String url1= "https://bizweb.dktcdn.net/100/116/589/products/led-bulb-day-toc-rang-dong.png?v=1617004880860";
+                Glide.with(holder.itemView.getContext())
+                        .load(url1)
+                        .into(holder.imgDevice);
+                break;
+        }
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test1/floors/rooms/devices/state");
+        holder.aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    
+                }
+            }
+        });
     }
 
     @Override
@@ -46,11 +83,13 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtNameDevice;
+        Switch aSwitch;
         ImageView imgDevice;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtNameDevice = itemView.findViewById(R.id.txtNameDevice);
             imgDevice = itemView.findViewById(R.id.imgDevice);
+            aSwitch = itemView.findViewById(R.id.sw);
         }
     }
 }
